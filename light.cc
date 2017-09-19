f1bec8cacbf4defb8a0338063626b5aa50a58fee
Adrian Biedrzycki <a@modules.io>
2017-09-19 06:41:16 -0700

    Small light.cc cleanup

--- light.cc
+++ light.cc
@@ -1,26 +1,9 @@
 #include "light.h"
 #include "util.h"
-#include "v8-strings.h"
-#include <node.h>
 #include <cmath>
 #include <vector>
 #include <queue>
 // #include <iostream>
-
-using v8::Context;
-using v8::Function;
-using v8::FunctionCallbackInfo;
-using v8::FunctionTemplate;
-using v8::Isolate;
-using v8::Local;
-using v8::Number;
-using v8::Object;
-using v8::Persistent;
-using v8::String;
-using v8::Value;
-using v8::Exception;
-using v8::Array;
-using v8::ArrayBuffer;
 
 class LightSource {
   public:
