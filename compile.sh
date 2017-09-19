dced9b9aa6eef19d433d74b35296d1d99be06580
Adrian Biedrzycki <a@modules.io>
2017-09-19 08:37:28 -0700

    Increase memory size in wasm build

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 mkdir -p build/wasm
-emcc --std=c++11 -s WASM=1 -O3 \
+emcc --std=c++11 -s WASM=1 -O3 -s TOTAL_MEMORY=134217728 \
   objectize.cc util.cc compose.cc tssl.cc light.cc \
   -o build/wasm/objectize.js
