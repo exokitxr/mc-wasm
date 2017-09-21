bc61e489e90c41b0a833f06b2d4b999e8920f051
Adrian Biedrzycki <a@modules.io>
2017-09-20 17:42:52 -0700

    Compile into a separate directory

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-mkdir -p build/wasm
+mkdir -p bin
 emcc --std=c++11 -s WASM=1 -O3 -s TOTAL_MEMORY=134217728 \
   util.cc FastNoise.cpp noise.cc cachedNoise.cc compose.cc march.cc tssl.cc light.cc heightfield.cc flod.cc noiser.cc objectize.cc \
-  -o build/wasm/objectize.js
+  -o bin/objectize.js
