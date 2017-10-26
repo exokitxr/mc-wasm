85e2d6e58d445a0713f46f6327c0d77d6bf24b63
Adrian Biedrzycki <a@modules.io>
2017-10-26 00:00:38 -0700

    Add compile module wrapping

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 mkdir -p bin
 emcc --std=c++11 -s WASM=1 -O3 -s TOTAL_MEMORY=268435456 \
   util.cc FastNoise.cpp noise.cc cachedNoise.cc compose.cc march.cc tssl.cc light.cc heightfield.cc flod.cc noiser.cc cull.cc objectize.cc \
+  --pre-js prefix.js --post-js postfix.js \
   -o bin/objectize.js
