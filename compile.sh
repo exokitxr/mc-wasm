d426ac0531e25f0d6c7f235acaf128d02ed1c300
Avaer Kazmer <a@modules.io>
2019-07-30 11:44:43 -0400

    Rename objectize -> mc

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@ mkdir -p bin
 emcc --std=c++11 -s WASM=1 -O3 -s TOTAL_MEMORY=268435456 \
   march.cc objectize.cc \
   --pre-js prefix.js --post-js postfix.js \
-  -o bin/objectize.js
+  -o bin/mc.js
 
 #  util.cc FastNoise.cpp noise.cc cachedNoise.cc compose.cc march.cc tssl.cc light.cc heightfield.cc flod.cc noiser.cc cull.cc objectize.cc \
 #  --pre-js prefix.js --post-js postfix.js \
